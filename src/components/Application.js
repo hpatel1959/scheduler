4a28c09f64e27d0cb5249f4cc49457b437878e70
Harsh Patel <hpatel1959@hotmai.com>
2023-03-27 15:53:51 -0400

    remove hardcoded days data from Application.js and use axios get req to acquire data instead

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "components/Application.scss";
 import DayList from "./DayList";
 import "components/Appointment"
@@ -47,25 +48,13 @@ const appointments = {
 export default function Application(props) {
   const [currentDay, setCurrentDay ] = useState('Monday');
 
-  const days = [
-    {
-      id: 1,
-      name: "Monday",
-      spots: 2,
-    },
-    {
-      id: 2,
-      name: "Tuesday",
-      spots: 5,
-    },
-    {
-      id: 3,
-      name: "Wednesday",
-      spots: 0,
-    },
-  ];
+  const [days, setDays] = useState([]);
 
-  ;
+  useEffect(() => {
+    axios.get('/api/days')
+      .then(res => setDays(res.data));
+  }, [])
+
 
   const appointmentsArr = Object.values(appointments).map(appointment => {
     return (
